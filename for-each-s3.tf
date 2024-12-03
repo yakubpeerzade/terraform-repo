c8f79f8aa69d7c800bf464cee372e12878a118ad
Yaqoob Peerzade <yaqoob.peerzade@zs.com>
2024-12-03 22:17:18 +0530

    adde new file for S3

---
+++ for-each-s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "mys3bucket" {
+
+    for_each = {
+      "dev" = "my-dapp-bucket-xyz"
+      "qa" = "my-qapp-bucket-xyz"
+      "stg" = "my-sapp-bucket-xyz"
+      "prdo" = "my-papp-bucket-xyz"
+    }
+
+    bucket = "${each.key}-${each.value}"
+    acl = "private"
+
+    tags = {
+      Environment = each.key
+      bucketname =  "${each.key}-${each.value}"
+      eachvalue = each.value
+    }
+
+  
+}
